7f777f7703df8fc0f7a76747b7dd860d9d4166fa
Shivam Pandey <pandeyshivam8765@gmail.com>
2025-11-10 08:42:34 +0530

    mlflow tracking completed

--- Network_security/Components/model_trainer.py
+++ Network_security/Components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import mlflow
 from Network_security.Exception.exception import NetworkSecurityException
 from Network_security.Logging.logging import logging
 from Network_security.Constants import training_pipeline
@@ -27,6 +28,18 @@ class ModelTrainer:
             self.data_transformation_artifact= data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+
+
+    def track_mlflow(self,best_model,classificationmetric):
+            with mlflow.start_run():
+                f1_score=classificationmetric.f1_score
+                precision_score=classificationmetric.precision_score
+                recall_score=classificationmetric.recall_score
+                mlflow.log_metric("f1_score", f1_score)
+                mlflow.log_metric("precision", precision_score)
+                mlflow.log_metric("recall_score", recall_score)
+                mlflow.sklearn.log_model(best_model, "model")
     def train_model(self,x_train, y_train, x_test, y_test):
         
         models= {
@@ -77,9 +90,11 @@ class ModelTrainer:
         best_model= models[best_model_name]
         y_train_pred= best_model.predict(x_train)
         classification_train_metric= get_classification_score(y_true=y_train, y_pred=y_train_pred)
+        self.track_mlflow(best_model,classification_train_metric)
         
         y_test_pred= best_model.predict(x_test)
         classification_test_metric= get_classification_score(y_test,y_test_pred)
+        self.track_mlflow(best_model, classification_test_metric)
                                         
         preprocessor=load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path= os.path.dirname(self.model_trainer_config.trained_model_file_path)
